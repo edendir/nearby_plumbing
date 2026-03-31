10c7dc806f51aebe5612aebd41040e246f5953e6
Robbie Lannon <robbielannon@gmail.com>
2026-03-31 14:39:05 -0500

    added logos and header copy, updated contact nav

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-import os
+import os, requests
 from flask import Flask, render_template
 from flask_mail import Message
 
 app = Flask(__name__)
+request = requests.Request()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/contact", methods=["POST"])
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    name = request.form.get("name")
-    email = request.form.get("email")
-    service = request.form.get("service")
-    message = request.form.get("message")
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        service = request.form.get("service")
+        message = request.form.get("message")
+        return render_template("contact.html")
+    else:
+        return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
