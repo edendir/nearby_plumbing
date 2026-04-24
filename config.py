532c15bedcae9f27a5939ef47ae84b9ab454af25
Robbie Lannon <robbielannon@gmail.com>
2026-04-24 15:43:39 -0500

    Revert "config file no longer ignored"
    This reverts commit 2554bdff76fe87aa2c533a3edea48cce515f93a4.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    ENV = os.environ.get('ENV', 'development')
-    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
-    MAIL_FROM = os.environ.get('MAIL_FROM')
-    MAIL_TO = os.environ.get('MAIL_TO')
